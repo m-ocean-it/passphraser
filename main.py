3e8f6d73503b514a7f6e2718b9f3309fe05c91a9
Maxim Motyshen <m.motyshen@gmail.com>
2022-02-27 15:19:54 +0300

    Save vertical space

--- main.py
+++ main.py
@@ -11,18 +11,15 @@ def to_passphrase(
         wordlist_option: str = 'BIP39') -> str:
 
     binary_str = bin(int(hex_str, 16))[2:]
-
     wordlist = get_wordlist(wordlist_option)
     chunk_size = get_max_fitting_degree_of_two(len(wordlist))
 
     binary_chunks = []
     for i in range(0, len(binary_str), chunk_size):
         chunk = binary_str[i:i+chunk_size].rjust(chunk_size, '0')
-
         binary_chunks.append(chunk)
 
     integers = [int(x, 2) for x in binary_chunks]
-
     words = [wordlist[i] for i in integers]
 
     return ' '.join(words)
@@ -33,14 +30,11 @@ def from_passphrase(
         wordlist_option: str = 'BIP39') -> str:
 
     words = passphrase.split()
-
     wordlist = get_wordlist(wordlist_option)
-
     integers = [wordlist.index(word)
                 for word in words]
 
     binary_chunks = [Binary(i) for i in integers]
-
     binary_str = ''.join(binary_chunks)
 
     hex_num = binToHex(binary_str)
@@ -57,18 +51,14 @@ def get_wordlist(wordlist_option: str = 'BIP39'):
 
 def Binary(n):
     s = bin(n)
-
     # removing "0b" prefix
     s1 = s[2:]
-
     return s1
 
 
 def binToHex(n):
-
     # convert binary to int
     num = int(n, 2)
-
     # convert int to hexadecimal
     hex_num = hex(num)
     return hex_num
