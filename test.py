ac2650423c761a19b719e2b5d333fdbc9ede7543
Maxim Motyshen <m.motyshen@gmail.com>
2022-03-01 23:25:16 +0300

    Update test module
    Use logging and generate ASCII string instead of hex

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ from termcolor import colored
 
 from main import from_passphrase, to_passphrase
 
-WORDLIST_OPTIONS = ('BIP39', 'EFF')
+TEST_LOG_PATH = 'test.log'
+
+WORDLIST_OPTIONS = ('BIP39', 'EFF', 'Norvig')
 
 
 def main():
@@ -13,7 +15,7 @@ def main():
 def test_for_symmetry():
     for _ in range(50):
 
-        test_data = get_random_hex_str()
+        test_data = get_random_str()
 
         print('TESTING FOR SYMMETRY')
         print('====================', end='\n\n')
@@ -28,16 +30,20 @@ def test_for_symmetry():
                                         wordlist_option=wordlist_option,
                                         verbose=True)
 
-            assert recovered == test_data, colored(
-                f'Should have recovered: {test_data}', 'red')
+            error_msg = f'Should have recovered:\n{test_data}\ninstead of\n{recovered}\n'
+            if recovered != test_data:
+                with open(TEST_LOG_PATH, 'a') as f:
+                    f.write(f'{error_msg}\n')
+                raise AssertionError(
+                    f'\n\n{error_msg}\nLogged to {TEST_LOG_PATH}')
             print(f'<=== TEST {i} PASSED', end='\n\n')
 
         print(colored('HOORRAY! SYMMETRY IS PRESERVED.', 'green'))
 
 
-def get_random_hex_str():
-    length = random.randint(1, 80)
-    chars = '0123456789abcdef'
+def get_random_str():
+    length = random.randint(1, 100)
+    chars = tuple(chr(i) for i in range(256))
 
     arr = []
     for _ in range(length):
