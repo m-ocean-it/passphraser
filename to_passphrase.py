c0c6030c24cfb8350c3c048d834db9abf114acb5
Maxim Motyshen <m.motyshen@gmail.com>
2022-03-02 18:52:42 +0300

    Major update for to_passphrase script

--- to_passphrase.py
+++ to_passphrase.py
@@ -1,9 +1,47 @@
 #!/bin/python3
 
+import argparse
 import sys
 
 from main import to_passphrase
 
-hex_str = sys.stdin.readline()
-passphrase = to_passphrase(hex_str)
-print(passphrase, file=sys.stdout)
+
+def main():
+    parser = argparse.ArgumentParser(description='description...')
+    parser.add_argument('input', nargs='?', default=None)
+    parser.add_argument('-m', '--mode', default='hex',
+                        help='specify how to parse input')
+    parser.add_argument('-w', '--wordlist', default='bip39')
+    parser.add_argument('-v', '--verbose', action='store_true')
+
+    args = parser.parse_args()
+
+    stdin_present = stdin_is_present()
+    if args.input:
+        input_str = args.input
+        if stdin_present:
+            print('[Warning] Ignoring stdin since argument was provided.')
+    else:
+        if not stdin_present:
+            print('Enter the value:')
+        input_str = sys.stdin.readline().strip()
+        if not input_str:
+            print('[Error] No input provided.')
+            sys.exit(1)
+
+    passphrase = to_passphrase(input_str,
+                               mode=args.mode,
+                               wordlist_option=args.wordlist,
+                               verbose=args.verbose)
+
+    print(f'[Result] {passphrase}', file=sys.stdout)
+
+
+def stdin_is_present():
+    if not sys.stdin.isatty():
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    main()
